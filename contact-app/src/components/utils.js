a9e0b0b39c3f6c0e91daec03046cdd416f538de4
Aniket <moteaniket952@gmail.com>
2025-01-15 13:03:47 +0530

    latest code

---
+++ contact-app/src/components/utils.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function util() {
+  return (
+    <div className="ui fixed menu">
+      <div className="ui container center">
+        <h2>util Manager</h2>
+      </div>
+    </div>
+  );
+}
+
+export default util;

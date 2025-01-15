b35ccabcd05e9e45cde142fe2ef4b0016731216e
Aniket <moteaniket952@gmail.com>
2025-01-16 01:51:59 +0530

    updated code

--- contact-app/src/components/Header.js
+++ contact-app/src/components/Header.js
@@ -5,6 +5,8 @@ function Header() {
     <div className="ui fixed menu">
       <div className="ui container center">
         <h2>Contact Manager</h2>
+        <h2>Phone Manager</h2>
+        <h2>hii</h2>
       </div>
     </div>
   );

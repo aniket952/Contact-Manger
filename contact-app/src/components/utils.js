ca1f45bc2b42e842db05a30448b0208e0c017bed
Aniket <moteaniket952@gmail.com>
2025-01-15 13:07:01 +0530

    dev code

--- contact-app/src/components/utils.js
+++ contact-app/src/components/utils.js
@@ -5,6 +5,8 @@ function util() {
     <div className="ui fixed menu">
       <div className="ui container center">
         <h2>util Manager</h2>
+        <h2>hii Manager</h2>
+        <h2>ths is your Manager</h2>
       </div>
     </div>
   );
